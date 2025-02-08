868ef9fd5b0dd6644c11da71a947acdbd4dabff0
Ovidiu <ovidiunoc@gmail.com>
2025-02-08 20:44:12 +0200

    Added grouping to pinpoints

--- LaravelReactApp/resources/js/Components/Map.jsx
+++ LaravelReactApp/resources/js/Components/Map.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useRef, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import { useEffect, useRef, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'leaflet.markercluster';
+import { Circle } from 'react-leaflet';
 
 const Map = ({ posts }) => {
   const [userLocation, setUserLocation] = useState(null);
-
   const mapRef = useRef();
+  const markerClusterGroup = useRef(L.markerClusterGroup());
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -22,53 +26,48 @@ const Map = ({ posts }) => {
     }
   }, []);
 
-  const MapEvents = () => {
-    const map = useMap();
+  useEffect(() => {
+    if (mapRef.current) {
+      mapRef.current.addLayer(markerClusterGroup.current);
+    }
+  }, [posts]);
 
-    useEffect(() => {
-      if (userLocation) {
-        const circle = L.circle(userLocation, {
-          radius: 3000,
-          color: "blue",
-          fillColor: "blue",
-          fillOpacity: 0.2,
-        }).addTo(map);
+  useEffect(() => {
+    if (posts.length > 0) {
+      markerClusterGroup.current.clearLayers();
+      posts.forEach((post) => {
+        if (post.latitude && post.longitude) {
+          const marker = L.marker([post.latitude, post.longitude]);
+          marker.bindPopup(
+            `<strong>${post.user?.name}</strong><br />${post.content}`
+          );
+          markerClusterGroup.current.addLayer(marker);
+        }
+      });
+    }
+  }, [posts]);
 
-        return () => {
-          map.removeLayer(circle);
-        };
-      }
-    }, [userLocation, posts, map]);
-
-    return null;
-  };
-
-  if (!userLocation) return <div className="text-white">Can't get your location...</div>;
+  if (!userLocation) return <div>Can't get your location...</div>;
 
   return (
     <MapContainer
       center={userLocation}
       zoom={12}
-      style={{ height: "100%", width: "100%" }}
+      style={{ height: '100%', width: '100%' }}
       whenCreated={(map) => (mapRef.current = map)}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-
-      <MapEvents />
-
-      {posts.map((post) =>
-        post.latitude && post.longitude ? (
-          <Marker key={post.id} position={[post.latitude, post.longitude]}>
-            <Popup>
-              <strong>{post.user?.name}</strong> <br />
-              {post.content}
-            </Popup>
-          </Marker>
-        ) : null
-      )}
+      
+      <Circle
+        center={userLocation}
+        radius={4000}
+        color="blue"
+        fillColor="blue"
+        fillOpacity={0.2}
+      />
     </MapContainer>
   );
 };
